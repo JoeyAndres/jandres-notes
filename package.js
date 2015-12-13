841a327cc860fa0ce0fb5aad1de8c49cbde139f0
Joey Arnold <jandres@ualberta.ca>
2015-12-12 21:28:01 -0700

    added summary and git link

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'jandres:notes',
-  version: '0.1.2',
+  version: '0.1.3',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Sticky note module for meteor, handling both front-end and back-end (db stuff).',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/JoeyAndres/jandres-notes.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
